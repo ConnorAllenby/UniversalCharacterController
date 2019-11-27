da939c2f37664756ab046ad6866cccaf6c67f086
ConnorAllenby <allenbyconnor@gmail.com>
2019-11-27 22:15:46 +0000

    InitalCommit
    Initial Character Controller Commit.
    Includes:
    - Base State Machine for movement.
    -Base State
    - Sprint State
    - Jump State
    - Running State
    - Idle State.
    - PlayerController

---
+++ PlayerStateMachine/Player_SprintState.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_SprintState : PlayerBaseState
+{
+    PlayerController playerRef;
+
+    public override void EnterState(PlayerController player)
+    {
+        Debug.Log("Player State = Sprinting");
+    }
+    public override void ExitState(PlayerController player)
+    {
+    }
+    public override void UpdateState(PlayerController player)
+    {
+        
+
+    }
+}
